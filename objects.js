406dd4145a55a75bc3f9d7854a56d69871eaa017
Houssein Abil <milameet@gmail.com>
2018-05-15 09:29:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,3 +5,7 @@ var playlist = {
 function updatePlaylist(playlist, artist, song){
   return playlist[artist] = song;
 }
+
+function removeFromPlaylist(playlist, artist){
+  return delete playlist[artist];
+}
